f7c5714fa5d3dce5cd40e082e98f9eb94acb6d8a
Shuai <40193779+mashuai191@users.noreply.github.com>
2019-11-13 06:23:47 -0800

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -155,8 +155,8 @@ class Config(object):
 
     # Shape of output mask
     # To change this you also need to change the neural network mask branch
-    #MASK_SHAPE = [28, 28]
-    MASK_SHAPE = [56, 56]
+    MASK_SHAPE = [28, 28]
+    #MASK_SHAPE = [56, 56]
 
     # Maximum number of ground truth instances to use in one image
     MAX_GT_INSTANCES = 100
